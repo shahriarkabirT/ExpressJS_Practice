064c00d15f72ec7f7b359ef4b9b613b8446e3f4c
Shahriar Kabir Tashin <122079836+shahriarkabirT@users.noreply.github.com>
2024-07-04 00:51:45 +0600

    router topic added

--- Learned_Topics/6_Router/Routers/publicRouter.js
+++ Learned_Topics/6_Router/Routers/publicRouter.js
@@ -1,7 +1,49 @@
 const express = require('express');
-const app = express();
+const publicRouter = express.Router();
 
-
-app.get('/',(req,res)=>{
+publicRouter.get('/',(req,res)=>{
     res.send("homepage");
 })
+    //outdated
+
+// publicRouter.param('user',(req,res,next,id)=>{
+//     if(id == 1) req.user = 'admin';
+//     else req.user = "annonymous";
+//     next();
+// })
+
+// publicRouter.param((param,option)=>(req,res,next,id)=>{
+//     if(id == option){
+//         next();
+//     }
+//     else{
+//         res.sendStatus(403);
+//     }
+// })
+
+// publicRouter.param('user','12');
+
+
+// publicRouter.get('/:user',(req,res)=>{
+//     res.send("hello " + req.user);
+// })
+
+    //new
+
+publicRouter.get('/:id',(req,res,next)=>{
+    const id = req.params.id;
+    if(id == 1){
+        req.user = 'admin';
+       
+    }
+    else{
+        req.user = 'anonymous';
+    }
+    next();
+
+},(req,res)=>{
+    res.send("hello " + req.user);
+})
+
+
+module.exports = publicRouter;
--- Learned_Topics/6_Router/index.js
+++ Learned_Topics/6_Router/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const app = express();
+const publicRouter = require('./Routers/publicRouter');
+const adminRouter = require('./Routers/adminRouter')
 
 
+const log = (req,res,next) =>{
+    console.log("logging");
+    next();
+}
 
-app.use('/admin',adminRouter);
+app.all('*', log);
 
-
-
+app.use('/', publicRouter);
+app.use('/admin', adminRouter);
 // Start the server
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+app.listen(3001, () => {
+    console.log(`App listening on port ${3001}`);
 });
