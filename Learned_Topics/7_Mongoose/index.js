a09926a19c0fd0cd8b867cf72b3239c69a0b0b7b
Shahriar Kabir Tashin <122079836+shahriarkabirT@users.noreply.github.com>
2024-07-08 16:19:03 +0600

    new added

--- Learned_Topics/7_Mongoose/index.js
+++ Learned_Topics/7_Mongoose/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/shahriar');
+  await mongoose.connect('mongodb://localhost:/todos');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
